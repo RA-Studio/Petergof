48b69b5a3eddc3387e36632f208eab403ecfce6c
TsvetkovLaptop <dstvetkov@gmail.com>
2019-10-24 12:47:20 +0300

    Закончил верстку Особенностей с слайдером

--- js/main.js
+++ js/main.js
@@ -1,4 +1,67 @@
 $(document).ready(function () {
+    resizewindow();
+    $(window).resize(function(e){
+        resizewindow();
+    });
+
+    if($('body').is('.features-page')) {
+        const sliderCount = $('.slider__item').length;
+        const slider = $('.slider-wrapper');
+        const slider2 = $('.left-wrapper-slider');
+        const progressBar = $('.progress');
+        progressBar.css('background-size', '100%' + 100 / sliderCount + '%');
+
+        slider.on('wheel', (function(e) {
+            e.preventDefault();
+
+            if (e.originalEvent.deltaY < 0) {
+                $(this).slick('slickPrev');
+            } else {
+                $(this).slick('slickNext');
+            }
+        }));
+        slider.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+            let calc = ( (nextSlide+1) / (slick.slideCount) ) * 100;
+            if(screen.width > 768){
+                progressBar
+                    .css('background-size', '100%' + calc + '%')
+                    .attr('aria-valuenow', calc );
+            } else {
+                progressBar
+                    .css('background-size', calc + '%' + '100%')
+                    .attr('aria-valuenow', calc );
+            }
+
+
+        });
+        slider.slick({
+            arrows: false,
+            vertical: true,
+            infinite: false,
+            dots: false,
+            asNavFor: slider2,
+            responsive: [
+                {
+                    breakpoint: 769,
+                    settings: {
+                        arrows: true,
+                        vertical: false
+                    }
+                }
+
+            ]
+        });
+        slider2.slick({
+            arrows: false,
+            vertical: false,
+            infinite: false,
+            dots: false,
+            fade: true,
+            draggable: false,
+            swipe: false,
+        });
+    }
+
     $( ".hamburger-header" ).click(function() {
         let modalMenu = $(".modal-menu");
         console.log(modalMenu);
@@ -11,3 +74,11 @@ $(document).ready(function () {
         }
     });
 });
+function resizewindow() {
+    if(screen.width < 769) {
+        $(document).find('.progress').css('width', $(document).find('.right-wrapper').width() + 'px');
+    } else {
+        $(document).find('.progress').css('width', 3 + 'px');
+    }
+};
+
